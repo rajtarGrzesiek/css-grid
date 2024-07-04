c1aaab86b7efd01bd02e65482be0b8d6c0ec2f91
Grzesiek <rajtar.grzesiek@gmail.com>
2024-07-04 13:51:50 +0200

    Second and propably last commit. Added functions for closing mobile navigation when coming back from res. higher than 992px.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
 const footerYear = document.querySelector(".footer__year");
 // ⏬ zmienna przechowująca wszystkie linki w nawigacji mobilnej
-const navLinks = document.querySelectorAll(".nav__link");
+const navLinks = document.querySelectorAll(".nav__link-mobile");
 
 const handleNav = () => {
 	navBtn.classList.toggle("is-active");
@@ -41,3 +41,23 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
+
+const mediaQuery = window.matchMedia("(min-width: 992px)");
+
+function closeMenu() {
+	navBtn.classList.remove("is-active");
+	navMobile.classList.remove("nav-mobile--active");
+}
+
+function handleBreakpointChange(e) {
+	if (e.matches) {
+		// Gdy szerokość okna jest większa niż 992px, zamknij menu
+		closeMenu();
+	}
+}
+
+// Dodajemy nasłuchiwanie na zmiany szerokości okna
+mediaQuery.addEventListener("change", handleBreakpointChange);
+
+// Sprawdzamy początkowy stan
+handleBreakpointChange(mediaQuery);
